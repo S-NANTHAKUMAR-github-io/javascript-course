2766133579f885ea10fe4bac15fdbd26826f8c18
Nantha Kumar <nandhavijay21@gmail.com>
2023-03-07 13:29:26 +0530

    commit

---
+++ Introduction/script.js
@@ -0,0 +1,16 @@
+//arrays
+// let cities = ["chennai", "trichy", "madurai"]
+// let marks = [78, 85, 96, 54 ,77 ]
+// console.log(marks.length);
+// 
+//array methods 
+// let arr1 = [5,8,10,7,9,11]
+// console.log(arr1.splice(3,3,17,20,40));
+// 
+// console.log(arr1.unshift(10));
+
+//if else 
+ 
+
+
+
